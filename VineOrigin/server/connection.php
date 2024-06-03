435bc0a33499e2d53b83831f9bc187367f47fe20
jayvick09 <157486840+jayvick09@users.noreply.github.com>
2024-06-03 13:45:41 +0800

    Delete VineOrigin directory

--- VineOrigin/server/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost","root","","vine-origin")
-    or die("could'nt connect to database");
-
-
-?>
--- VineOrigin/server/get_cashoes.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('connection.php');
-
-$stmt = $conn->prepare("SELECT * FROM products WHERE category = 'cashoes' AND status = 'a' LIMIT 3");
-$stmt->execute();
-$cashoes_products = $stmt->get_result();
-?>
