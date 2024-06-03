edd558f692ee21b77b2541cb8563e5c8968b1e7d
jayvick09 <157486840+jayvick09@users.noreply.github.com>
2024-06-04 01:56:15 +0800

    Delete VineOrigin directory

--- VineOrigin/visitor_landing/server/get_featured_products.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('connection.php');
-
-$stmt = $conn->prepare("SELECT * FROM products WHERE category = 'featured' AND status = 'a' LIMIT 3");
-$stmt->execute();
-$featured_products = $stmt->get_result();
-?>
